71e501fbe4051293f0ffde11388c7bd3cdf77fe6
Nikolay <nikolain073@gmail.com>
2021-01-04 18:17:13 +0200

    Create a Category model to our posts

--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,23 @@
 from django.db import models
+from django.db.models.deletion import CASCADE, SET_NULL
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    """ Model for category of our Post (e.g. Sport, Fashion etc.) """
+    """ Model for category of our Post (e.g. Sport, Fashion etc.)
+        Our posts can have only one category.
+    """
 
-    name = models.CharField(max_length=100, help_text="Write a category for post (e.g. Sport, Fashion etc.)")
+    name = models.CharField(verbose_name="category", max_length=100, help_text="Write a category for post (e.g. Sport, Fashion etc.)")
+
+
+    class Meta: 
+        verbose_name_plural = "Category"
 
     def __str__(self):
         return self.name
-
-
-
-
 
 
 class Post(models.Model):
@@ -23,6 +26,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     date_posted = models.DateTimeField(default=timezone.now)
 
 
