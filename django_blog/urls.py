1286dcee52ff2f544dfd67fc540b42d996f72087
Nikolay <nikolain073@gmail.com>
2021-01-04 10:31:28 +0200

    Add path to blog-home temlate

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
