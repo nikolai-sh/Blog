2004fa6ac1e1348c6010cefd92b4c899f49697dc
Nikolay <nikolain073@gmail.com>
2021-01-05 01:02:16 +0200

    Delete old home func view

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post
 
-def home(request):
-    return render(request, 'blog/blog_home.html')
 
 class PostListView(ListView):
     
