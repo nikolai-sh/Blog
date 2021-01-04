aeda899fbc64c41a92525100f2d1316e3c120bd7
Nikolay <nikolain073@gmail.com>
2021-01-04 22:29:11 +0200

    Create PostListView

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
+from blog.models import Post
 from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic import ListView
+from .models import Post
 
 def home(request):
     return render(request, 'blog/blog_home.html')
+
+class PostListView(ListView):
+    
+    model = Post
+    template_name = "blog/blog_home.html"
+    ordering = ['-date_posted']
+    
