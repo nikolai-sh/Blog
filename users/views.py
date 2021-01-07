8586e264c53d46b5c0c2170454b7661ac469720b
Nikolay <nikolain073@gmail.com>
2021-01-07 11:50:28 +0200

    Bugfix

--- users/views.py
+++ users/views.py
@@ -11,6 +11,6 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
-        else:
-            form = UserRegisterForm()
-        return render(request, 'users/register.html', {'form': form})
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
