f8af2fad714cb33727859325a8e6e73ce7dd3abe
Nikolay <nikolain073@gmail.com>
2021-01-07 15:54:49 +0200

    Create profile view

--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def sign_up(request):
+    """ Function to ctreate new user account """
+
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
@@ -14,3 +17,9 @@ def sign_up(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/sign_up.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    """ Function to create user profile """
+    return render('user/profile.html')
