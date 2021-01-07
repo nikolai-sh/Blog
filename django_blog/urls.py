eb1441956b540025e3c0c084edefc09525126885
Nikolay <nikolain073@gmail.com>
2021-01-07 16:23:08 +0200

    Add profile url

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -25,11 +25,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/blog/', permanent=True)),
     path('sign_up/', user_views.sign_up, name='sign_up'),
-    path('sign_in/', auth_views.SignInView.as_view(template_name='users/sign_in.html'), name='sign_in'),
-    path('sign_out/', auth_views.SignOutView.as_view(template_name='users/sign_out.html'), name='sign_out'),
+    path('sign_in/', auth_views.LoginView.as_view(template_name='users/sign_in.html'), name='sign_in'),
+    path('sign_out/', auth_views.LogoutView.as_view(template_name='users/sign_out.html'), name='sign_out'),
+    path('profile/', user_views.profile, name='profile'),
     path('blog/', include('blog.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
