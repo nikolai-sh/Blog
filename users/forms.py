f848125711cb44412da16cb0368b97cb9cb14c4d
Nikolay <nikolain073@gmail.com>
2021-01-08 16:27:07 +0200

    Create UserUpdateForm

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserModel
 from django.forms import fields
 
 
@@ -10,4 +10,14 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    """ Class to update Profile model """
+
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
 
