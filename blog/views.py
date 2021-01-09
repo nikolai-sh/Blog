962e48edf0ca433a1e3383f3ec777691ab27ffeb
Nikolay <nikolain073@gmail.com>
2021-01-09 11:11:07 +0200

    Add categories to view and in blog-home

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from blog.models import Post
+from blog.models import Post, Category
 from django.shortcuts import render
 from django.views.generic import (ListView, 
                                   DetailView,
@@ -15,9 +15,18 @@ class PostListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        categories = Category.objects.all()
+        context['categories'] = categories
         # context['most_popular_posts'] = Post.objects.order_by('visit_post')[:3] #TODO get three most popular posts
         return context
+    
 
 class PostDetailView(DetailView):
     model = Post
     
+
+# class CategoryListView(ListView):
+#     model = Category
+#     template_name = "blog/blog_home.html"
+#     context_object_name = 'categories'
+#     print(Category.objects.all())
