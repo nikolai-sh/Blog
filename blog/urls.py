e6752d2581e5e6c83efbeff7794c3cf2db72dfc3
Nikolay <nikolain073@gmail.com>
2021-01-09 14:29:53 +0200

    Add to url CategoryPostDetailView

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (PostListView, 
                     PostDetailView,
+                    CategoryPostDetailView
                     )
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('category/<int:pk>/<str:name>/', CategoryPostDetailView.as_view(), name='posts-category'),
 ]
