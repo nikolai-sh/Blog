eedc4ef2d005aad9d371c7934edf8186882c6eff
Nikolay <nikolain073@gmail.com>
2021-01-10 22:20:51 +0200

    Add delete post url

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from .views import (PostListView,
                     PostDetailView,
                     PostCreateView,
                     PostUpdateView,
+                    PostDeleteView,
                     CategoryPostDetailView,
                     UserPostListView
                     )
@@ -13,6 +14,7 @@ urlpatterns = [
     path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('new/', PostCreateView.as_view(), name='post-create'),
     path('<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('category/<int:pk>/<str:name>/', CategoryPostDetailView.as_view(), name='posts-category'),
 ]
