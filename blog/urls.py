4c4d6c6e58cdfadf59e20c3922a621e2874fa92b
Nikolay <nikolain073@gmail.com>
2021-01-10 10:36:11 +0200

    Add user posts url

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views import (PostListView, 
                     PostDetailView,
-                    CategoryPostDetailView
+                    CategoryPostDetailView,
+                    UserPostListView
                     )
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('category/<int:pk>/<str:name>/', CategoryPostDetailView.as_view(), name='posts-category'),
 ]
