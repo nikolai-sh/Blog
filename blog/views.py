d5608a98e5df050e218f7803b3d5c8c953e69a03
Nikolay <nikolain073@gmail.com>
2021-01-10 10:36:28 +0200

    Create userpost list view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from blog.models import Post, Category
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.views.generic import (ListView, 
                                   DetailView,
                                   )
@@ -17,18 +18,17 @@ class PostListView(ListView):
         context = super().get_context_data(**kwargs)
         categories = Category.objects.all()
         context['categories'] = categories
-        # context['most_popular_posts'] = Post.objects.order_by('visit_post')[:3] #TODO get three most popular posts
         return context
     
 
 class PostDetailView(DetailView):
+   
     model = Post
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         categories = Category.objects.all()
-        context['categories'] = categories
-    
+        context['categories'] = categories  
         return context
     
 
@@ -41,6 +41,25 @@ class CategoryPostDetailView(DetailView):
 
         context = super().get_context_data(**kwargs)
         categories = Category.objects.all()
-        context['categories'] = categories
-       
+        context['categories'] = categories      
         return context
+    
+
+
+class UserPostListView(ListView):
+    
+    model = Post
+    template_name = "blog/user_posts.html"
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = Category.objects.all()
+        context['categories'] = categories
+        return context
+  
