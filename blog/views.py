b07582ef5b2decdf5b2fd9b2561c55cd9a9fab45
Nikolay <nikolain073@gmail.com>
2021-01-10 22:20:29 +0200

    Create post delete view

--- blog/views.py
+++ blog/views.py
@@ -6,57 +6,67 @@ from django.contrib.auth.models import User
 from django.views.generic import (ListView, 
                                   DetailView,
                                   CreateView,
-                                  UpdateView
+                                  UpdateView,
+                                  DeleteView
                                   )
 
+
 class CategoryMixin(ContextMixin):
-    """ Add all categories to context """
-    
+    """ Add all categories to context """  
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["categories"] = Category.objects.all()
         return context
     
 
-class PostListView(ListView, CategoryMixin):
-    
+class PostListView(ListView, CategoryMixin): 
     model = Post
     template_name = "blog/blog_home.html"
     context_object_name = 'posts'
     paginate_by = 5
    
 
-class PostDetailView(DetailView, CategoryMixin):
-   
+class PostDetailView(DetailView, CategoryMixin):  
     model = Post
 
 
 class PostCreateView(LoginRequiredMixin, CreateView, CategoryMixin):
-   
     model = Post
     fields = ['title', 'content', 'category']
     
     def form_valid(self, form):
         """ Setting the author before create post create form"""
-
         form.instance.author = self.request.user
         return super().form_valid(form)
 
 
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView, CategoryMixin):
-   
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin,
+                     UpdateView, CategoryMixin
+                     ):
     model = Post
     fields = ['title', 'content', 'category']
     
     def form_valid(self, form):
-        """ Setting the author before create post create form"""
-
+        """ Setting the author before create post create form """
         form.instance.author = self.request.user
         return super().form_valid(form)
     
     def test_func(self):
-        """ Check if user is author of post """
+        """ Check if user is author of post and return bool value """
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin,
+                     DeleteView, CategoryMixin
+                     ):  
+    model = Post
+    success_url = '/' #redirect to home page after delete post
+
+    def test_func(self):
+        """ Check if user is author of post and return bool value """
         post = self.get_object()
         if self.request.user == post.author:
             return True
@@ -64,13 +74,11 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView, Catego
 
 
 class CategoryPostDetailView(DetailView, CategoryMixin):
-
     model = Category
     template_name = "blog/posts_category.html" 
 
 
-class UserPostListView(ListView, CategoryMixin ):
-    
+class UserPostListView(ListView, CategoryMixin ):  
     model = Post
     template_name = "blog/user_posts.html"
     context_object_name = 'posts'
