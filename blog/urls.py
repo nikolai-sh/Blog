f3b2381f9d5cf437d4a3bf24db5701b373bdbef4
Nikolay <nikolain073@gmail.com>
2021-01-10 21:23:53 +0200

    Add post update url

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (PostListView, 
                     PostDetailView,
                     PostCreateView,
+                    PostUpdateView,
                     CategoryPostDetailView,
                     UserPostListView
                     )
@@ -11,6 +12,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('new/', PostCreateView.as_view(), name='post-create'),
+    path('<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('category/<int:pk>/<str:name>/', CategoryPostDetailView.as_view(), name='posts-category'),
 ]
