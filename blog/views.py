70169d47e74ec64db014c00daf7092044ab0049d
Nikolay <nikolain073@gmail.com>
2021-01-10 10:38:43 +0200

    Delete spaces

--- blog/views.py
+++ blog/views.py
@@ -38,7 +38,6 @@ class CategoryPostDetailView(DetailView):
     template_name = "blog/posts_category.html"
 
     def get_context_data(self, **kwargs):
-
         context = super().get_context_data(**kwargs)
         categories = Category.objects.all()
         context['categories'] = categories      
