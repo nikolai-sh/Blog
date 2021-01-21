0ebdf4887658d0fddd1ae1ade5e5d7948c1c70ab
Nikolay <nikolain073@gmail.com>
2021-01-21 17:34:46 +0200

    Add searchresultsview url

--- blog/urls.py
+++ blog/urls.py
@@ -5,12 +5,14 @@ from .views import (PostListView,
                     PostUpdateView,
                     PostDeleteView,
                     CategoryPostDetailView,
-                    UserPostListView
+                    UserPostListView,
+                    SearchResultsView
                     )
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('search/', SearchResultsView.as_view(), name='search-results'),
     path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('new/', PostCreateView.as_view(), name='post-create'),
     path('<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
