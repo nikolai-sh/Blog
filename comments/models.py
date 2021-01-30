862f5cf21cf349d178169e9d63fc589fbb2e62a3
Nikolay <nikolain073@gmail.com>
2021-01-30 21:29:17 +0200

    Create Comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from blog.models import Post
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, related_name='comments')
+    body = models.TextField(max_length=500)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE)
+    
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return f'Comment {self.body} by {self.user}'
+
+    # def children(self):
+    #     return Comment.objects.filter(parent=self)
+
+    # @property
+    # def is_parent(self):
+    #     if self.parent is not None:
+    #         return False
+    #     return True
