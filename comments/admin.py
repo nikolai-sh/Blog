1ca48733c5d6d7c58932066cb436f1f483caf58a
Nikolay <nikolain073@gmail.com>
2021-01-31 14:02:35 +0200

    Add comment model to admin

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post
+from .models import Comment
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'body', 'post', 'pub_date', 'active','parent')
+    list_filter = ('active', '-created_on')
+    search_fields = ('user', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
