0dd3066f399fe37b7ce5b1e0e56173ca1e839c18
Nikolay <nikolain073@gmail.com>
2021-02-11 18:10:06 +0200

    Preper project to deploy on heroku

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'p*8smkq04st=n@t&=r1vwz45ea1n997&@vql01r)+f2ngpad6$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nikolai-sh-blog.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
