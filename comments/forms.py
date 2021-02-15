85c36acba973e2b69748d5d6d1a38122298991e4
Nikolay <nikolain073@gmail.com>
2021-02-15 14:51:19 +0200

    Create comment models and forms

--- comments/forms.py
+++ comments/forms.py
@@ -4,8 +4,7 @@ from .models import Comment
 
 class CommentForm(forms.ModelForm):
     """ Class to create comment form """
-    # template_name = 'blog/post_detail.html'
    
     class Meta:
         model = Comment
-        fields = ['body']
+        fields = ['author', 'text']
